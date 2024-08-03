0d702d46de9ff193cd3371c94f5ffb06e3351b18
Heng_Xin <282000500@qq.com>
2024-08-03 20:34:45 +0800

    手撕无栈协程

--- src/stepHX.cpp
+++ src/stepHX.cpp
@@ -1,83 +1,47 @@
-#include <chrono>
-#include <coroutine>
-#include <thread>
-#include "rbtree.hpp"
-#include "debug.hpp"
+#include <cstdio>
+#include <iostream>
 
-using namespace std::chrono_literals;
+using ll = long long; // 为了突出重点, 就不使用模版了qwq
 
-struct RepeatAwaiter {
-    bool await_ready() const noexcept { 
-        return false;
+struct FibTask {
+    int state = 0;
+    ll a, b, c;
+
+    ll fib() {
+        switch (state) {
+        case 0: // 第一次进入
+            a = b = 1;
+            state = 1;
+            goto st_01;
+        case 1: // 第二次进入
+            state = 2;
+            goto st_02;
+        case 2: // 第三次进入
+            state = 3;
+            goto st_03;
+        case 3: // 第4次及其以后的进入
+            goto st_04;
+        }
+
+    st_01:
+        return a;
+    st_02:
+        return b;
+    st_03:
+        while (1) {
+            c = a + b; // 我们不能使用局部变量作为 c, 因为 c 是在恢复的时候还用到的
+            return c;
+            st_04:
+            b = a;
+            a = c;
+        }
     }
-
-    std::coroutine_handle<> await_suspend(std::coroutine_handle<> coroutine) const noexcept {
-        return std::noop_coroutine();
-    }
-
-    void await_resume() const noexcept {}
 };
-
-template<class T>
-struct Promise {
-    auto initial_suspend() {
-        return std::suspend_always();
-    }
-
-    auto final_suspend() noexcept {
-        return std::suspend_always();
-    }
-
-    void unhandled_exception() {
-        throw;
-    }
-
-    auto yield_value(T ret) {
-        mRetValue = ret;
-        return RepeatAwaiter();
-    }
-
-    void return_void() {
-        mRetValue = 0;
-    }
-
-    std::coroutine_handle<Promise> get_return_object() {
-        return std::coroutine_handle<Promise>::from_promise(*this);
-    }
-
-    T mRetValue;
-};
-
-template<class T>
-struct Task {
-    using promise_type = Promise<T>;
-
-    Task(std::coroutine_handle<promise_type> coroutine)
-        : mCoroutine(coroutine) {}
-
-    std::coroutine_handle<promise_type> mCoroutine;
-};
-
-template<class T>
-Task<T> fib() {
-    T a = 1, b = 1;
-    co_yield a;
-    co_yield b;
-    while (1) {
-        T c = a + b;
-        co_yield c;
-        a = b;
-        b = c;
-    }
-}
 
 int main() {
-    // 实现一个 生成器
-    using ll = long long;
-    auto task = fib<ll>();
+    FibTask f;
     while (1) {
-        task.mCoroutine.resume();
-        std::cout << task.mCoroutine.promise().mRetValue << '\n';
+        std::cout << f.fib() << '\n';
         getchar();
     }
     return 0;
