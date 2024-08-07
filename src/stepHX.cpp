f77227d6023eda86bad79a415abb4ddf10118028
Heng_Xin <282000500@qq.com>
2024-08-07 14:41:41 +0800

    学习epoll作为协程loop

--- src/stepHX.cpp
+++ src/stepHX.cpp
@@ -5,21 +5,27 @@
 #include <queue>
 #include <string>
 #include <thread>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <fcntl.h>
+#include <array>
+#include <vector>
 
 #include "HX/Task.hpp"
 
-using namespace HX;
+using namespace std::chrono;
 
-struct Loop {
-    void addTask(std::coroutine_handle<> coroutine) {
-        _taskQueue.emplace(coroutine);
-    }
-
+class TimerLoop {
     void addTimer(
         std::chrono::system_clock::time_point expireTime, 
         std::coroutine_handle<> coroutine
     ) {
         _timer.insert({expireTime, coroutine});
+    }
+
+public:
+    void addTask(std::coroutine_handle<> coroutine) {
+        _taskQueue.emplace(coroutine);
     }
 
     /**
@@ -51,17 +57,53 @@ struct Loop {
         }
     }
 
-    static Loop& getLoop() {
-        static Loop loop;
+    static TimerLoop& getLoop() {
+        static TimerLoop loop;
         return loop;
     }
 
 private:
-    explicit Loop() : _timer()
-                    , _taskQueue()
+    /**
+     * @brief 暂停者
+     */
+    struct SleepAwaiter { // 使用 co_await 则需要定义这 3 个固定函数
+        bool await_ready() const noexcept { // 暂停
+            return false;
+        }
+
+        void await_suspend(std::coroutine_handle<> coroutine) const { // `await_ready`后执行: 添加计时器
+            TimerLoop::getLoop().addTimer(_expireTime, coroutine);
+        }
+
+        void await_resume() const noexcept { // 计时结束
+        }
+
+        std::chrono::system_clock::time_point _expireTime; // 过期时间
+    };
+
+public:
+    /**
+     * @brief 暂停指定时间点
+     * @param expireTime 时间点, 如 2024-8-4 22:12:23
+     */
+    HX::Task<void> static sleep_until(std::chrono::system_clock::time_point expireTime) {
+        co_await SleepAwaiter(expireTime);
+    }
+
+    /**
+     * @brief 暂停一段时间
+     * @param duration 比如 3s
+     */
+    HX::Task<void> static sleep_for(std::chrono::system_clock::duration duration) {
+        co_await SleepAwaiter(std::chrono::system_clock::now() + duration);
+    }
+
+private:
+    explicit TimerLoop() : _timer()
+                         , _taskQueue()
     {}
 
-    Loop& operator=(Loop&&) = delete;
+    TimerLoop& operator=(TimerLoop&&) = delete;
 
     /// @brief 计时器红黑树
     std::multimap<std::chrono::system_clock::time_point, std::coroutine_handle<>> _timer;
@@ -70,141 +112,34 @@ private:
     std::queue<std::coroutine_handle<>> _taskQueue;
 };
 
-/**
- * @brief 暂停者
- */
-struct SleepAwaiter { // 使用 co_await 则需要定义这 3 个固定函数
-    bool await_ready() const noexcept { // 暂停
-        return false;
-    }
 
-    void await_suspend(std::coroutine_handle<> coroutine) const { // `await_ready`后执行: 添加计时器
-        Loop::getLoop().addTimer(_expireTime, coroutine);
-    }
-
-    void await_resume() const noexcept { // 计时结束
-    }
-
-    std::chrono::system_clock::time_point _expireTime; // 过期时间
+class EpollLoop {
+    
 };
 
-/**
- * @brief 暂停指定时间点
- * @param expireTime 时间点, 如 2024-8-4 22:12:23
- */
-Task<void> sleep_until(std::chrono::system_clock::time_point expireTime) {
-    co_await SleepAwaiter(expireTime);
-}
-
-/**
- * @brief 暂停一段时间
- * @param duration 比如 3s
- */
-Task<void> sleep_for(std::chrono::system_clock::duration  duration) {
-    co_await SleepAwaiter(std::chrono::system_clock::now() + duration);
-}
-
-template <class T = void> 
-struct NonVoidHelper {
-    using Type = T;
-};
-
-template <> 
-struct NonVoidHelper<void> {
-    using Type = NonVoidHelper;
-
-    explicit NonVoidHelper() = default;
-};
-
-template <class A>
-concept Awaiter = requires(A a, std::coroutine_handle<> h) {
-    { a.await_ready() };
-    { a.await_suspend(h) };
-    { a.await_resume() };
-};
-
-template <class A>
-concept Awaitable = Awaiter<A> || requires(A a) {
-    { a.operator co_await() } -> Awaiter;
-};
-
-template <class A> 
-struct AwaitableTraits;
-
-template <Awaiter A> 
-struct AwaitableTraits<A> {
-    // C++11 std::declva 获取函数返回值类型
-    using RetType = decltype(std::declval<A>().await_resume());
-    using NonVoidRetType = NonVoidHelper<RetType>::Type;
-};
-
-template <class A>
-    requires(!Awaiter<A> && Awaitable<A>)
-struct AwaitableTraits<A>
-    : AwaitableTraits<decltype(std::declval<A>().operator co_await())> 
-{};
-
-
-struct WhenAnyAwaiter {
-    bool await_ready() const noexcept { // 暂停
-        return false;
-    }
-
-    void await_suspend(std::coroutine_handle<> coroutine) const { // `await_ready`后执行: 添加计时器
-        if (_tasks.empty()) return coroutine;
-            mControl.mPrevious = coroutine;
-        for (auto const &t: _tasks.subspan(0, mTasks.size() - 1))
-            t.mCoroutine.resume();
-        return _tasks.back().mCoroutine;
-    }
-
-    void await_resume() const noexcept { // 计时结束
-    }
-
-    std::span<Task<> const> _tasks;
-};
-
-template <class... Ts>
-Task<std::variant<typename AwaitableTraits<Ts>::RetType...>> WhenAny(Ts&&... args) {
-    std::size_t size = sizeof...(Ts);
-
-}
-
-using namespace std::chrono;
-
-Task<int> taskFun01() {
+HX::Task<int> taskFun01() {
     std::cout << "hello1开始睡1秒\n";
-    co_await sleep_for(1s); // 1s 等价于 std::chrono::seconds(1);
+    co_await TimerLoop::sleep_for(1s); // 1s 等价于 std::chrono::seconds(1);
     std::cout << "hello1睡醒了\n";
     std::cout << "hello1继续睡1秒\n";
-    co_await sleep_for(1s); // 1s 等价于 std::chrono::seconds(1);
+    co_await TimerLoop::sleep_for(1s); // 1s 等价于 std::chrono::seconds(1);
     std::cout << "hello1睡醒了\n";
     co_return 1;
 }
 
-Task<double> taskFun02() {
+HX::Task<double> taskFun02() {
     std::cout << "hello2开始睡2秒\n";
-    co_await sleep_for(2s);
+    co_await TimerLoop::sleep_for(2s);
     std::cout << "hello2睡醒了\n";
     co_return 11.4514;
 }
 
-Task<std::string> taskFun03() {
+HX::Task<std::string> taskFun03() {
     std::cout << "hello3开始睡0.5秒\n";
-    co_await sleep_for(500ms);
+    co_await TimerLoop::sleep_for(500ms);
     std::cout << "hello3睡醒了\n";
     co_return "好难qwq";
 }
-
-Task<void> co_main() {
-    auto v = co_await WhenAny(taskFun01(), taskFun02());
-}
-
-#include <sys/epoll.h>
-#include <sys/socket.h>
-#include <fcntl.h>
-#include <array>
-#include <vector>
 
 int main() {
     // 使用epoll监测stdin
@@ -242,15 +177,15 @@ int main() {
     auto task_01 = taskFun01();
     auto task_02 = taskFun02();
     auto task_03 = taskFun03();
-    Loop::getLoop().addTask(task_01);
-    Loop::getLoop().addTask(task_02);
-    Loop::getLoop().addTask(task_03);
-    Loop::getLoop().runAll();
+    TimerLoop::getLoop().addTask(task_01);
+    TimerLoop::getLoop().addTask(task_02);
+    TimerLoop::getLoop().addTask(task_03);
+    TimerLoop::getLoop().runAll();
     std::cout << "看看01: " << task_01._coroutine.promise().result() << '\n';
     std::cout << "看看02: " << task_02._coroutine.promise().result() << '\n';
     std::cout << "看看03: " << task_03._coroutine.promise().result() << '\n';
 #elif 1
-    co_main();
+
 #endif
     return 0;
 }
